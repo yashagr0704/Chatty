50bf7c237a9a5e403419c0443257932aa44175f8
yashagr0704 <yashagr070403@gmail.com>
2023-09-15 22:04:32 +0530

    changed host

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chatty-mek7.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
